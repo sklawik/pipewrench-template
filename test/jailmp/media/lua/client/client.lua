d3fd2cde36109d8cb1bca8a74abe7fcf9b3e6082
root <root@homepc.>
2025-01-09 04:49:21 +0100

    fixes in files

--- test/jailmp/media/lua/client/client.lua
+++
@@ -1,7 +0,0 @@
-local ____exports = {}
-local ____pipewrench_2Devents = require('lua_modules/@asledgehammer/pipewrench-events/PipeWrench-Events')
-local Events = ____pipewrench_2Devents.default
-Events.onPlayerMove:addListener(function(player)
-    print("Hello from Pipewrench Template! Modify the code as you need. Have a great start with modern javascript.")
-end)
-return ____exports
